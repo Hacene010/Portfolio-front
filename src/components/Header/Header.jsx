7d6eaf226a16831a0c469630be7514b626d597e8
Hacene010 <hamrounehacene06@gmail.com>
2021-07-17 10:36:11 +0200

    worked on the header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SHeader from './Style';
+
+export default function Header() {
+  return (
+    <SHeader className="Header">
+      <div className="burger-container">
+        <nav>
+          <ul>
+            <Link to="about">
+              <li>A propos</li>
+            </Link>
+
+            <Link to="career">
+              <li>Parcours</li>
+            </Link>
+            <Link to="/">
+              <div className="logo">
+                <img src="/img/logo.png" alt="logo" />
+              </div>
+            </Link>
+            <Link to="achievments">
+              <li>Réalisations</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </SHeader>
+  );
+}
