d05bf7a8bd15d9f8fafff5886fb38f9ce827b3f5
Hacene010 <hamrounehacene06@gmail.com>
2021-07-17 12:31:49 +0200

    added an aboutMe component

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,5 +1,35 @@
 import SAbout from './Style';
 
 export default function About() {
-  return <SAbout id="about" />;
+  return (
+    <SAbout id="about">
+      <div className="container">
+        <div className="myPic">
+          <img src="/img/AboutMe/MyPic.jpeg" alt="myPicture" />
+        </div>
+
+        <div className="desc-container">
+          <div className="title">
+            <h1>Pour en savoir plus sur hacene</h1>
+          </div>
+          <div className="intro">
+            <p>
+              Je m&apos;appelle Hacene HAMROUNE. Je suis en formation à la Wild
+              Code School. Une chance de reconversion après un parcours
+              universitaire atypique. Développeur web est, pour moi, un métier
+              qui fait sens et dans lequel j&apos;ai envie de m&apos;investir.
+              &ldquo;Apprendre à apprendre&ldquo; est la formule qui a orienté
+              mon choix vers la Wild Code School. L&apos;autonomie, la rigueur
+              et le respect de l&apos;autre font également partie des valeurs
+              qui me tiennent à cœur. Aujourd&apos;hui, je cherche une
+              alternance au sein d’une entreprise à taille humaine qui me
+              permettra de m&apos;épanouir. Mon esprit d&apos;analyse, mon sens
+              de l’observation et ma créativité sauront m&apos;aider à réussir
+              et monter encore plus en compétences !
+            </p>
+          </div>
+        </div>
+      </div>
+    </SAbout>
+  );
 }
--- src/components/AboutMe/Style.jsx
+++ src/components/AboutMe/Style.jsx
@@ -1,4 +1,100 @@
 import styled from 'styled-components';
 
-const SAbout = styled.div``;
+const SAbout = styled.div`
+  height: 94vh;
+  background-color: #415164;
+
+  .myPic {
+    margin-top: 0;
+
+    img {
+      border-radius: 8px;
+      height: 90vh;
+    }
+
+    animation-name: pic;
+    animation-duration: 700ms;
+    animation-timing-function: ease-out;
+    @keyframes pic {
+      from {
+        opacity: 0;
+        margin-left: -100%;
+        margin-top: -50%;
+      }
+      to {
+        opacity: 1;
+        margin-left: 0;
+        margin-top: 0;
+      }
+    }
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: inherit;
+  }
+
+  .desc-container {
+    background-color: #6e7b8b9f;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 50vw;
+    height: 85vh;
+    text-align: center;
+    border-radius: 50%;
+
+    animation-name: desc;
+    animation-duration: 700ms;
+    animation-timing-function: ease-in-out;
+    @keyframes desc {
+      from {
+        margin-top: -50%;
+        margin-right: -100%;
+      }
+      to {
+        margin-top: 0;
+        margin-right: 0;
+      }
+    }
+  }
+
+  h1 {
+    color: #9ed0c9;
+    font-size: 2.5em;
+    font-weight: 900;
+    text-transform: uppercase;
+  }
+
+  .intro {
+    width: 90%;
+    margin: 50px;
+    padding-top: 50px;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .title {
+    width: 500px;
+
+    animation-name: title;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+
+    @keyframes title {
+      from {
+        opacity: 0.5;
+        margin-top: 200%;
+      }
+      to {
+        opacity: 1;
+        margin-top: 0;
+      }
+    }
+  }
+`;
+
 export default SAbout;
